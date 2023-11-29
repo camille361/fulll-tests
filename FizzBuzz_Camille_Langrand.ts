5abbbffb6f028ff8dea001c77f6dae45c736ab4d
Camille Langrand <39941320+camille361@users.noreply.github.com>
2023-11-29 12:04:48 +0100

    Update FizzBuzz_Camille_Langrand.ts

--- FizzBuzz_Camille_Langrand.ts
+++ FizzBuzz_Camille_Langrand.ts
@@ -2,12 +2,11 @@ const N: number = 100;
 
 if (N > 0) { // On vérifie que N soit positif
 	for (let i: number = 1; i <= N; i++) {
-  		// Utilisation de l'opérateur ternaire pour gérer chaque cas
-  		const output: string | number =  (i % 3 === 0 && i % 5 === 0) ? 'FizzBuzz' :
-    			 	   					 (i % 3 === 0) ? 'Fizz' :
-    				   					 (i % 5 === 0) ? 'Buzz' :
-    				   					  i;
-
+		// Utilisation de l'opérateur ternaire pour gérer chaque cas
+  		const output: string | number = (i % 3 === 0 && i % 5 === 0) ? 'FizzBuzz' :
+    			 	   		(i % 3 === 0) ? 'Fizz' :
+    				   		(i % 5 === 0) ? 'Buzz' :
+    				   		i;
   		console.log(output); // Affichage du résultat
 	}
 } else {
